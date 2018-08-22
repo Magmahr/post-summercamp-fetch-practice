02de9a89c0e2b7bfe7d0eb041416e151a3107005
matt maher <mattmaher0@gmail.com>
2018-08-22 14:52:09 -0400

    done

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'B
 const addMembersTrigger = document.getElementById('add-members-trigger')
 
 function addRemaningRadioheadMembers() {
-
+radioheadRemainingMembers.map(member => {
+  fetch("http://localhost:3000/members", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: member,
+      band_id: 2
+      })
+    }).then(res => res.json().then(member=>console.log(member.id)))
+  })
 }
+
+
+
+// function log(){
+//   let members = fetch("http://localhost:3000/members").then(resp => resp.json()).then(data => [...data]).then(data => console.log(members.keys))
+//
+//   // members.map(member => {console.log(member.id)})
+// }
 
 addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
